88a3476381a264563ad02bbe43369747f702c178
TeteuTias <mateus2.0@icloud.com>
2025-02-25 20:59:56 -0300

    resolvendo bug
    esqueci de conectar no db

--- lib/models/src/certificatesHandler/get/GetEvent.ts
+++ lib/models/src/certificatesHandler/get/GetEvent.ts
@@ -1,9 +1,12 @@
 import { ObjectId } from "mongodb"
 import EventCertificateModel from "@/lib/models/EventCertificateModel"
 import { IEventCertificate } from "@/lib/models/EventCertificateModel"
-
+import { connectToDatabase } from "@/lib/mongodb"
+//
+//
 export default async function GetEvent(eventId: ObjectId): Promise<IEventCertificate | null> {
 
+    await connectToDatabase
     const data = await EventCertificateModel.findOne({
         _id: eventId
     })
